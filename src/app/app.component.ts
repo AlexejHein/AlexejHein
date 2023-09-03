d8b63177010a20d0b5122a7af85737adfe2cae4b
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-09-03 18:02:45 +0200

    add imgsilder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'alexejhein';
+export class AppComponent implements OnInit {
+  images = ['bild1.jpg', 'bild2.jpg', 'bild3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImages();
+  }
+
+
+  updateImages() {
+   setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      },10);
+    }, 8000);
+  }
+
+
+
+
 }
